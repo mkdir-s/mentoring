1d893fe1f6d1225bcd056a0e3ff411b67e722999
ihtml <i.html06@gmail.com>
2023-05-28 02:14:35 +0600

    feat: add registr section, fix program and add burger menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,7 @@
-AOS.init();
+const burgerButton = document.querySelector('.header__burger'),
+      header = document.querySelector('.header');
+
+      
+burgerButton.addEventListener('click', (e) => {
+  header.classList.toggle('open');
+})
