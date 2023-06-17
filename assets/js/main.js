50e6ab55c99350da57e4c4dc16630170ba8918e7
ihtml <i.html06@gmail.com>
2023-06-17 14:51:29 +0600

    feat: add reviews section

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,3 +5,29 @@ const burgerButton = document.querySelector('.header__burger'),
 burgerButton.addEventListener('click', (e) => {
   header.classList.toggle('open');
 })
+
+const swiper = new Swiper('.reviews__slider', {
+  loop: true,
+  slidesPerView: 2.5,
+  spaceBetween: 30,
+  pagination: {
+    el: '.reviews__swiper-pagination',
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1.3,
+      spaceBetween: 20
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 3,
+      spaceBetween: 40
+    }
+  }
+});
